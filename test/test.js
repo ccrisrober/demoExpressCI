7ac5b13f4f0154f430d74df4d6324f87ed6849b0
Cristian Rodríguez <ccrisrober@gmail.com>
2016-08-09 23:03:42 +0200

    Update test

--- test/test.js
+++ test/test.js
@@ -11,6 +11,7 @@ describe("SAMPLE UNIT TEST", function() {
 			.expect(200)
 			.end(function(err, res) {
 				res.status.should.equal(200);
+				
 				var data = res.body.data;
 				data.should.equal("ok");
 				done();
